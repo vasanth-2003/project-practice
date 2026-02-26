a5f64a64e8ddbc05939836f3096fcde083f11753
vasanth-2003 <vasanthservey@gmail.com>
2026-02-26 11:59:31 +0530

    new changes happen in customer file

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Customer = () => {
   return (
     <div className='font-semibold font-mono flex flex-col justify-center items-center min-h-screen'>
-        <h1>Im from Customer</h1>
-        <p>Im accessing customer file from setting branch</p>
+        <h1>Im accessing customer file from setting branch</h1>
+        {/* <p>Im accessing customer file from setting branch</p> */}
     </div>
   )
 }
