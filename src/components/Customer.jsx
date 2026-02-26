ca224689491b70e643c040382d9f8ed189d3e165
vasanth-2003 <vasanthservey@gmail.com>
2026-02-26 12:13:10 +0530

    make initial changes back

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Customer = () => {
   return (
     <div className='font-semibold font-mono flex flex-col justify-center items-center min-h-screen'>
-        <h1>Im accessing customer file from setting branch</h1>
+        <h1>I'm from customer</h1>
         {/* <p>Im accessing customer file from setting branch</p> */}
     </div>
   )
